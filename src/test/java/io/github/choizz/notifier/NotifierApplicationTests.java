cefeaedc0328f6d09c50773409a66fc4060b865c
choizz156 <tkfka156@gmail.com>
2026-05-22 10:53:50 +0900

    Initial commit

---
+++ src/test/java/io/github/choizz/notifier/NotifierApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.choizz.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
