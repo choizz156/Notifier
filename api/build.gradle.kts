d3a4bfc775b1a452b57a378b3197a8cb17bd6a8f
choizz156 <tkfka156@gmail.com>
2026-05-22 11:38:45 +0900

    feat: api 모듈 빌드 설정 및 프로파일 환경 구성

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,4 +1,14 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
