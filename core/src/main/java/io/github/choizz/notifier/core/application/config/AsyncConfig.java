172d44cf8b62a087bebfaaabd90199f19505c1dd
choizz156 <tkfka156@gmail.com>
2026-05-24 03:59:40 +0900

    feat: 가상 스레드(Virtual Threads) 활성화 및 AsyncConfig 적용

--- core/src/main/java/io/github/choizz/notifier/core/application/config/AsyncConfig.java
+++ core/src/main/java/io/github/choizz/notifier/core/application/config/AsyncConfig.java
@@ -4,8 +4,9 @@ import java.util.concurrent.Executor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
@@ -13,12 +14,8 @@ public class AsyncConfig {
 
 	@Bean(name = "taskExecutor")
 	public Executor taskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(2);
-		executor.setMaxPoolSize(5);
-		executor.setQueueCapacity(100);
-		executor.setThreadNamePrefix("notification-publish-");
-		executor.initialize();
-		return executor;
+		return new TaskExecutorAdapter(
+			new VirtualThreadTaskExecutor("publish-async-")
+		);
 	}
 }
