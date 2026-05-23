c68bfc08e1d32daa2d58a8c9ff45312463a426d1
choizz156 <tkfka156@gmail.com>
2026-05-23 20:10:15 +0900

    refactor: PublicationContext로 이름 변경 및 빌더 패턴 적용

---
+++ core/src/main/java/io/github/choizz/notifier/core/application/dto/PublicationContext.java
@@ -0,0 +1,74 @@
+package io.github.choizz.notifier.core.application.dto;
+
+import io.github.choizz.notifier.core.domain.event.PublishCommandEvent;
+import io.github.choizz.notifier.core.domain.event.PublishCompletedEvent;
+import io.github.choizz.notifier.core.domain.util.JsonUtils;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+public final class PublicationContext {
+
+	private final Long notificationId;
+	private final Long subscriberId;
+	private final String notificationType;
+	private final String channel;
+	private final String metadata;
+	private final String failReason;
+	private int retryCount;
+
+	@Builder(toBuilder = true)
+	private PublicationContext(
+		Long notificationId,
+		Long subscriberId,
+		String notificationType,
+		String channel,
+		String metadata,
+		String failReason,
+		int retryCount
+	) {
+
+		this.notificationId = notificationId;
+		this.subscriberId = subscriberId;
+		this.notificationType = notificationType;
+		this.channel = channel;
+		this.metadata = metadata;
+		this.failReason = failReason;
+		this.retryCount = retryCount;
+	}
+
+	public static PublicationContext success(PublishCompletedEvent event) {
+
+		return PublicationContext.builder()
+			.notificationId(event.notificationId())
+			.notificationType(event.notificationType())
+			.channel(event.channel())
+			.metadata(event.metadata())
+			.retryCount(0)
+			.build();
+	}
+
+	public  PublicationContext notSent(String failReason) {
+	return 	this.toBuilder()
+			.failReason(failReason)
+			.build();
+	}
+
+	public static PublicationContext of(PublishCommandEvent event) {
+
+		return PublicationContext.builder()
+			.notificationId(event.notificationId())
+			.subscriberId(event.subscriberId())
+			.notificationType(event.notificationType())
+			.channel(event.channel())
+			.metadata(JsonUtils.toJson(event.metadata()))
+			.retryCount(0)
+			.build();
+	}
+
+	public void increaseRetryCount(int retryCount) {
+		this.retryCount = retryCount + 1;
+	}
+}
