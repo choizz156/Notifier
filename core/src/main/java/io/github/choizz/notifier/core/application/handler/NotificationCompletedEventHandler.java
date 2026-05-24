fb3e255a0323f5b7cd40d0b15495f5e1aefb7d18
choizz156 <tkfka156@gmail.com>
2026-05-25 06:23:24 +0900

    refactor: 알람 성공 시 상태 변경 동기 처리

--- core/src/main/java/io/github/choizz/notifier/core/application/handler/NotificationCompletedEventHandler.java
+++ core/src/main/java/io/github/choizz/notifier/core/application/handler/NotificationCompletedEventHandler.java
@@ -3,6 +3,7 @@ package io.github.choizz.notifier.core.application.handler;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.choizz.notifier.core.application.dto.PublicationContext;
 import io.github.choizz.notifier.core.application.port.in.NotificationEventLogUseCase;
@@ -20,16 +21,17 @@ public class NotificationCompletedEventHandler {
 
 	private final NotificationEventLogUseCase notificationEventLogUseCase;
 	private final NotificationUseCase notificationUseCase;
-
+	
 	@Async("taskExecutor")
+	@Transactional
 	@EventListener
-	public void updateNotification(PublishCompletedEvent event) {
-		notificationUseCase.updateStatus(event.notificationId(), NotificationStatus.COMPLETED);
-	}
-
-	@Async("taskExecutor")
-	@EventListener
-	public void updateNotificationLog(PublishCompletedEvent event) {
-		notificationEventLogUseCase.saveEventLog(event.notificationId(), EventStatus.SENT, PublicationContext.success(event));
+	public void handleNotificationCompleted(PublishCompletedEvent event) {
+		try {
+			notificationUseCase.updateStatus(event.notificationId(), NotificationStatus.COMPLETED);
+			notificationEventLogUseCase.saveEventLog(event.notificationId(), EventStatus.SENT, PublicationContext.success(event));
+		} catch (Exception e) {
+			log.error("알림(ID: {}) 상태 및 이벤트 로그 성공 업데이트에 실패하여 상태가 변경되지 않았습니다.", event.notificationId(), e);
+			throw new RuntimeException(e);
+		}
 	}
 }
