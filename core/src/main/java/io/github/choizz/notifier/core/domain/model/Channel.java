76ee45ba701e1c283836b77c73f1ff9331c1b558
choizz156 <tkfka156@gmail.com>
2026-05-27 07:25:55 +0900

    refactor: DLQ 복구 스케줄러 설정 추가 및 Channel 도메인 코드 정리

--- core/src/main/java/io/github/choizz/notifier/core/domain/model/Channel.java
+++ core/src/main/java/io/github/choizz/notifier/core/domain/model/Channel.java
@@ -3,18 +3,13 @@ package io.github.choizz.notifier.core.domain.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-public record Channel(@JsonValue String name) {
+public record Channel(@JsonValue String name) { // "channel": "IN_APP" 형식
 	
 	public static final Channel EMAIL = new Channel("EMAIL");
 	public static final Channel IN_APP = new Channel("IN_APP");
 
-	@JsonCreator
+	@JsonCreator// 생성자 사용 유도
 	public static Channel of(String name) {
 		return name != null ? new Channel(name.toUpperCase()) : null;
-	}
-
-	@Override
-	public String toString() {
-		return name;
 	}
 }
